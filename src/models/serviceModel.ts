57d282a8c3102266b4264bae44ae37fc6de584f2
Veerasiva <veerasiva01@gmail.com>
2025-01-26 10:47:29 +0530

    fix/service model: array of consumers and start end time of the service

--- src/models/serviceModel.ts
+++ src/models/serviceModel.ts
@@ -6,23 +6,30 @@ const serviceSchema = new mongoose.Schema({
     ref: 'Provider', 
     required: true,
   },
-  consumerId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Consumer', 
+  serviceType: {
+    type: String,
     required: true,
   },
+  description: {
+    type: String,
+    required: false,
+  },
+  startTime: {
+    type: Date,
+    required: true,
+  },
+  endTime: {
+    type: Date,
+    required: true,
+  },
+  consumers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Consumer',
+  }],
   status: {
     type: String,
     enum: ['Requested', 'Accepted', 'In Progress', 'Completed', 'Canceled', 'Rejected'],
     default: 'Requested',
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
   },
 }, { timestamps: true });
 
