9d12ce7832c042f563618d5d065952e66a803dde
Veerasiva <veerasiva01@gmail.com>
2025-01-28 22:52:56 +0530

    delete/lib and types: remove unwanted files

--- src/types/providerInfo.ts
+++
@@ -1,9 +0,0 @@
-export interface ProviderInfo {
-   providerId: string;
-   initialService: {
-      serviceType: string;
-      description: string;
-      startTime: string;
-      endTime: string;
-   }
-}
