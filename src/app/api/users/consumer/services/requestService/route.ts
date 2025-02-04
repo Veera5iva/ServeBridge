c28fa62cb7ad017324955401257fbc043f5d2628
Veerasiva <veerasiva01@gmail.com>
2025-02-04 17:42:36 +0530

    fix: remove requested service from available services on consumer dashboard

--- src/app/api/users/consumer/services/requestService/route.ts
+++ src/app/api/users/consumer/services/requestService/route.ts
@@ -1,17 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Consumer, Service } from "@/models";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
    try {
       const reqBody = await request.json();
-      let { serviceId, consumerId } = reqBody;
-
-      serviceId = new mongoose.Types.ObjectId(serviceId);
-      consumerId = new mongoose.Types.ObjectId(consumerId);
+      const { serviceId, consumerId } = reqBody;
       
-      await Service.findByIdAndUpdate(serviceId, { $addToSet: { requestedConsumers: consumerId, status: "Requested" } });
+      await Service.findByIdAndUpdate(serviceId, { $addToSet: { requestedConsumers: {consumer: consumerId, status: "Requested"}}});
       await Consumer.findByIdAndUpdate(consumerId, { $addToSet: { requestedServices: serviceId } });
 
       return NextResponse.json({ message: "Service requested successfully", success: true });
